e4462e63a5f156545e3e3c1c373b5b443f985844
SergeyShvaitser <sergiyshvaitser@gmail.com>
2018-11-19 12:09:03 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>Please see the Readme for details of this test.</div>
+        );
+    }
+}
+
+export default App;
