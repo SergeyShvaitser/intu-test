cbac60a2624b09173e51a65a1751674ac2abc0c0
SergeyShvaitser <sergiyshvaitser@gmail.com>
2018-11-19 14:00:05 +0200

    Add reusable Button common component

---
+++ src/views/common/Button.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+const Button = ({children, ...props}) => {
+  return (
+    <div>
+      <button {...props}>{children}</button>
+    </div>
+  )
+}
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired
+}
+
+export default Button;
