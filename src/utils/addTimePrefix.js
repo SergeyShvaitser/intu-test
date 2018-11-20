e5c30b20c717561ef49b26cbdd6bdc24100991ca
SergeyShvaitser <sergiyshvaitser@gmail.com>
2018-11-20 02:35:06 +0200

    Update formatting utils

--- src/utils/addTimePrefix.js
+++ src/utils/addTimePrefix.js
@@ -1,3 +1,8 @@
-export default function addPrefix(value) {
+export default function addPrefix(value, isMilliseconds) {
+    if(isMilliseconds && value < 10) {
+      return `00${value}`
+    } else if (isMilliseconds && value < 99) {
+      return `0${value}`
+    }
     return value < 10 ? `0${value}` : value
 };
--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -7,5 +7,5 @@ export default function formatTime(ms) {
       minutes = parseInt((ms / (1000 * 60)) % 60, 10),
       hours = parseInt((ms / (1000 * 60 * 60)) % 24, 10);
 
-  return `${addTimePrefix(hours)}:${addTimePrefix(minutes)}:${addTimePrefix(seconds)},${milliseconds}`;
+  return `${addTimePrefix(hours)}:${addTimePrefix(minutes)}:${addTimePrefix(seconds)},${addTimePrefix(milliseconds, true)}`;
 };
